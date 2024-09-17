f811e6fe3d9ceeb245a1e507fe3caae3181a521e
Alejo <scastaneda578@gmail.com>
2024-09-16 21:23:14 -0500

    Add a migration

--- GradeManagmentSystem-BackEnd/Migrations/20240917022112_update-tables.Designer.cs
+++ GradeManagmentSystem-BackEnd/Migrations/20240917022112_update-tables.Designer.cs
@@ -3,6 +3,7 @@ using GradeManagmentSystem_BackEnd.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GradeManagmentSystem_BackEnd.Migrations
 {
     [DbContext(typeof(AppGradesContext))]
-    partial class AppGradesContextModelSnapshot : ModelSnapshot
+    [Migration("20240917022112_update-tables")]
+    partial class updatetables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
