fa5f74f44c6188ba20f3d8d65a29a3eebd3fae07
paula torres <1003torrespaula@gmail.com>
2024-09-17 18:20:01 -0500

    detallitos

--- GradeManagmentSystem-BackEnd/Model/Attendant.cs
+++ GradeManagmentSystem-BackEnd/Model/Attendant.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public required string Name { get; set; }
-        public string LastName { get; set; }
-        public string Relationship { get; set; }
+        public required string LastName { get; set; }
+        public required string Relationship { get; set; }
     }
 }
