aebb5175948d9754542bbff110f21eff9e72765a
paula torres <1003torrespaula@gmail.com>
2024-09-17 18:13:24 -0500

    correccion a alejandro

---
+++ GradeManagmentSystem-BackEnd/Migrations/20240917231253_detallitos.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GradeManagmentSystem_BackEnd.Migrations
+{
+    /// <inheritdoc />
+    public partial class detallitos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- GradeManagmentSystem-BackEnd/Model/Attendant.cs
+++ GradeManagmentSystem-BackEnd/Model/Attendant.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public required string Name { get; set; }
-        public  string LastName { get; set; } = string.Empty;
-        public string Relationship { get; set; } = string.Empty;
+        public string LastName { get; set; }
+        public string Relationship { get; set; }
     }
 }
