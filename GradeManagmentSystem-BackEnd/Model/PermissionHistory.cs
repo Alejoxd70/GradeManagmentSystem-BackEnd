f1af14f2ee791818b4ea94619820bfa65c49e035
YUDY CORTES <yudycgomez@outlook.com>
2024-09-24 19:00:03 -0500

    add history,usety,permiss,permissUsTy

---
+++ GradeManagmentSystem-BackEnd/Model/PermissionHistory.cs
@@ -0,0 +1,11 @@
+﻿namespace GradeManagmentSystem_BackEnd.Model
+{
+    public class PermissionHistory
+    {
+        public int Id { get; set; }
+        public required string IdPermission { get; set; }
+        public required string PermissionName { get; set; }
+        public required DateTime Modified {  get; set; }
+        public required string User {  get; set; }
+    }
+}
---
+++ GradeManagmentSystem-BackEnd/Model/PermissionUserTypeHistory.cs
@@ -0,0 +1,13 @@
+﻿namespace GradeManagmentSystem_BackEnd.Model
+{
+    public class PermissionUserTypeHistory
+    {
+        public int Id { get; set; }
+        public required string IdPermissionUserType { get; set; }
+        public required string UserType { get; set; }
+        public required string Permission { get; set; }
+        public required DateTime Modified { get; set; }
+        public required string User { get; set; }
+
+    }
+}
