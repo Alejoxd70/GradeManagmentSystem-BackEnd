a0ab22e7acfe8f9953cf6184e985bb2482dbc920
YUDY CORTES <yudycgomez@outlook.com>
2024-09-25 19:39:11 -0500

    Add DbSet and Details

--- GradeManagmentSystem-BackEnd/Context/AppGradesContext.cs
+++ GradeManagmentSystem-BackEnd/Context/AppGradesContext.cs
@@ -15,9 +15,7 @@ namespace GradeManagmentSystem_BackEnd.Context
         //Añadi los siguientes DbSet
         public DbSet<UserType> UserTypes { get; set; }
         public DbSet<PermissionUserType> PermissionUserTypes { get; set; }
-
         public DbSet<Permission> Permissions { get; set; }
-
         public DbSet<Student> Students { get; set; }
         public DbSet<Attendant> Attendants { get; set; }
         public DbSet<Teacher> Teachers { get; set; }
@@ -27,33 +25,56 @@ namespace GradeManagmentSystem_BackEnd.Context
         public DbSet<Assigment> Assigments { get; set; }
         public DbSet<Grade> Grades { get; set; }
         public DbSet<SubjectTeacher> SubjectTeachers { get; set; }
+        //TABLES HISTORY
+        public DbSet<UserHistory> UserHistories { get; set; }
+        public DbSet<UserTypeHistory> UserTypeHistories { get; set; }
+        public DbSet<PermissionUserTypeHistory> PermissionUserTypeHistories { get; set; }
+        public DbSet<PermissionHistory> PermissionHistories { get; set; }
+        public DbSet<StudentHistory> StudentHistories { get; set; }
+        public DbSet<AttendantHistory> AttendantHistories { get; set; }
+        public DbSet<TeacherHistory> TeacherHistories { get; set; }
+        public DbSet<GroupYearHistory> GroupYearHistories { get; set; }
+        public DbSet<SubjectHistory> SubjectHistories { get; set; }
+        public DbSet<GroupHistory> GroupHistories { get; set; }
+        public DbSet<AssigmentHistory> AssigmentHistories { get; set; }
+        public DbSet<GradeHistory> GradeHistories { get; set; }
+        public DbSet<SubjectTeacherHistory> SubjectTeacherHistories { get; set; }
 
-       
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
+            //builders models
             modelBuilder.Entity<User>().HasKey(u => u.Id);
             modelBuilder.Entity<UserType>().HasKey(u => u.Id);
             modelBuilder.Entity<PermissionUserType>().HasKey(u => u.Id);
             modelBuilder.Entity<Permission>().HasKey(u => u.Id);
             modelBuilder.Entity<Student>().HasKey(u => u.Id);
-            //Añadi Builder
             modelBuilder.Entity<Attendant>().HasKey(u => u.Id);
             modelBuilder.Entity<Teacher>().HasKey(u => u.Id);
             modelBuilder.Entity<GroupYear>().HasKey(u => u.Id);
-
-            //añadi estos builders
             modelBuilder.Entity<Subject>().HasKey(u => u.Id);
             modelBuilder.Entity<Group>().HasKey(u => u.Id);
-
-
             modelBuilder.Entity<Assigment>().HasKey(u => u.Id);
             modelBuilder.Entity<Grade>().HasKey(u => u.Id);
             modelBuilder.Entity<SubjectTeacher>().HasKey(u => u.Id);
 
+            //BUILDERS HISTORY
 
+            modelBuilder.Entity<UserHistory>().HasKey(u => u.Id);
+            modelBuilder.Entity<UserTypeHistory>().HasKey(u => u.Id);
+            modelBuilder.Entity<PermissionUserTypeHistory>().HasKey(u => u.Id);
+            modelBuilder.Entity<PermissionHistory>().HasKey(u => u.Id);
+            modelBuilder.Entity<StudentHistory>().HasKey(u => u.Id);
+            modelBuilder.Entity<AttendantHistory>().HasKey(u => u.Id);
+            modelBuilder.Entity<TeacherHistory>().HasKey(u => u.Id);
+            modelBuilder.Entity<GroupYearHistory>().HasKey(u => u.Id);
+            modelBuilder.Entity<SubjectHistory>().HasKey(u => u.Id);
+            modelBuilder.Entity<GroupHistory>().HasKey(u => u.Id);
+            modelBuilder.Entity<AssigmentHistory>().HasKey(u => u.Id);
+            modelBuilder.Entity<GradeHistory>().HasKey(u => u.Id);
+            modelBuilder.Entity<SubjectTeacherHistory>().HasKey(u => u.Id);
 
 
         }
---
+++ GradeManagmentSystem-BackEnd/Migrations/20240926003221_DbsetHistory.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GradeManagmentSystem_BackEnd.Migrations
+{
+    /// <inheritdoc />
+    public partial class DbsetHistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AssigmentHistories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdAssigment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SubjectTeacher = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modified = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AssigmentHistories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AttendantHistories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdAttendant = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Relationship = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modified = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AttendantHistories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GradeHistories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdGrade = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Assigment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Student = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modified = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GradeHistories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GroupHistories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdGroup = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GroupName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modified = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GroupHistories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GroupYearHistories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdGroupYear = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Year = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Student = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Group = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modified = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GroupYearHistories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PermissionHistories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdPermission = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PermissionName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modified = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PermissionHistories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PermissionUserTypeHistories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdPermissionUserType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Permission = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modified = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PermissionUserTypeHistories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudentHistories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdStudent = table.Column<int>(type: "int", nullable: false),
+                    Student_code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    User = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modified = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentHistories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubjectHistories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdSubject = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Subjectname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modified = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubjectHistories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubjectTeacherHistories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdSubjectTeacher = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Teacher = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Subject = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GroupYear = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modified = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubjectTeacherHistories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "teacherHistories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdTeacher = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    User = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Specialization = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modified = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_teacherHistories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "userHistories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdUser = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Identification = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modified = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_userHistories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "userTypeHistories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdUserType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserTypeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modified = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_userTypeHistories", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AssigmentHistories");
+
+            migrationBuilder.DropTable(
+                name: "AttendantHistories");
+
+            migrationBuilder.DropTable(
+                name: "GradeHistories");
+
+            migrationBuilder.DropTable(
+                name: "GroupHistories");
+
+            migrationBuilder.DropTable(
+                name: "GroupYearHistories");
+
+            migrationBuilder.DropTable(
+                name: "PermissionHistories");
+
+            migrationBuilder.DropTable(
+                name: "PermissionUserTypeHistories");
+
+            migrationBuilder.DropTable(
+                name: "StudentHistories");
+
+            migrationBuilder.DropTable(
+                name: "SubjectHistories");
+
+            migrationBuilder.DropTable(
+                name: "SubjectTeacherHistories");
+
+            migrationBuilder.DropTable(
+                name: "teacherHistories");
+
+            migrationBuilder.DropTable(
+                name: "userHistories");
+
+            migrationBuilder.DropTable(
+                name: "userTypeHistories");
+        }
+    }
+}
