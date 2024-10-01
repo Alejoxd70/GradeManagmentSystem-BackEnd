f029fc4df00f79c03fbdafb48b161543a99e9b96
YUDY CORTES <yudycgomez@outlook.com>
2024-10-01 17:40:56 -0500

    add-triggers UserType_Permission_PermissionUserType

---
+++ GradeManagmentSystem-BackEnd/Migrations/20240926003937_Details.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GradeManagmentSystem_BackEnd.Migrations
+{
+    /// <inheritdoc />
+    public partial class Details : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_userTypeHistories",
+                table: "userTypeHistories");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_userHistories",
+                table: "userHistories");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_teacherHistories",
+                table: "teacherHistories");
+
+            migrationBuilder.RenameTable(
+                name: "userTypeHistories",
+                newName: "UserTypeHistories");
+
+            migrationBuilder.RenameTable(
+                name: "userHistories",
+                newName: "UserHistories");
+
+            migrationBuilder.RenameTable(
+                name: "teacherHistories",
+                newName: "TeacherHistories");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserTypeHistories",
+                table: "UserTypeHistories",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserHistories",
+                table: "UserHistories",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TeacherHistories",
+                table: "TeacherHistories",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserTypeHistories",
+                table: "UserTypeHistories");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserHistories",
+                table: "UserHistories");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TeacherHistories",
+                table: "TeacherHistories");
+
+            migrationBuilder.RenameTable(
+                name: "UserTypeHistories",
+                newName: "userTypeHistories");
+
+            migrationBuilder.RenameTable(
+                name: "UserHistories",
+                newName: "userHistories");
+
+            migrationBuilder.RenameTable(
+                name: "TeacherHistories",
+                newName: "teacherHistories");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_userTypeHistories",
+                table: "userTypeHistories",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_userHistories",
+                table: "userHistories",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_teacherHistories",
+                table: "teacherHistories",
+                column: "Id");
+        }
+    }
+}
---
+++ GradeManagmentSystem-BackEnd/Migrations/20241001213706_triggers-UseTy-Permiss-PermisUsTy.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GradeManagmentSystem_BackEnd.Migrations
+{
+    /// <inheritdoc />
+    public partial class triggersUseTyPermissPermisUsTy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            CREATE OR ALTER TRIGGER trg_AuditPermission
+            ON [Permissions]
+            AFTER INSERT, UPDATE, DELETE
+            AS
+            BEGIN
+                SET NOCOUNT ON;
+                -- If there are inserted or updated records
+                IF EXISTS (SELECT * FROM inserted)
+                BEGIN
+                    INSERT INTO PermissionHistories (IdPermission, PermissionName, Modified, ModifiedBy)
+                    SELECT i.Id, i.PermissionName,
+                        GETDATE(),
+                           CASE 
+                               WHEN EXISTS (SELECT * FROM deleted) THEN 'UPDATE' 
+                               ELSE 'INSERT' 
+                           END
+                    FROM inserted i;
+                END
+ 
+                -- If there are deleted records
+                IF EXISTS (SELECT * FROM deleted)
+                BEGIN
+                    INSERT INTO PermissionHistories (IdPermission, PermissionName, Modified, ModifiedBy)
+                    SELECT d.Id, d.PermissionName, GETDATE(), 'DELETE'
+                          FROM deleted d;
+                END
+            END;
+        ");
+
+            migrationBuilder.Sql(@"
+            CREATE OR ALTER TRIGGER trg_AuditPermissionUserType
+            ON [PermissionUserTypes]
+            AFTER INSERT, UPDATE, DELETE
+            AS
+            BEGIN
+                SET NOCOUNT ON;
+                -- If there are inserted or updated records
+                IF EXISTS (SELECT * FROM inserted)
+                BEGIN
+                    INSERT INTO PermissionUserTypeHistories (IdPermissionUserType , UserType, Permission, Modified, ModifiedBy)
+                    SELECT i.Id, i. UserTypeId, i.PermissionId, 
+                        GETDATE(),
+                           CASE 
+                               WHEN EXISTS (SELECT * FROM deleted) THEN 'UPDATE' 
+                               ELSE 'INSERT' 
+                           END
+                    FROM inserted i;
+                END
+ 
+                -- If there are deleted records
+                IF EXISTS (SELECT * FROM deleted)
+                BEGIN
+                    INSERT INTO PermissionUserTypeHistories (IdPermissionUserType, UserType, Permission, Modified, ModifiedBy)
+                    SELECT d.Id, d.UserTypeId, d.PermissionId, GETDATE(), 'DELETE'
+                          FROM deleted d;
+                END
+            END;
+        ");
+
+            migrationBuilder.Sql(@"
+            CREATE OR ALTER TRIGGER trg_AuditUserType
+            ON [UserTypes]
+            AFTER INSERT, UPDATE, DELETE
+            AS
+            BEGIN
+                SET NOCOUNT ON;
+                -- If there are inserted or updated records
+                IF EXISTS (SELECT * FROM inserted)
+                BEGIN
+                    INSERT INTO UserTypeHistories (IdUserType, UserTypeName, Modified, ModifiedBy)
+                    SELECT i.Id, i. UserTypeName,
+                        GETDATE(),
+                           CASE 
+                               WHEN EXISTS (SELECT * FROM deleted) THEN 'UPDATE' 
+                               ELSE 'INSERT' 
+                           END
+                    FROM inserted i;
+                END
+ 
+                -- If there are deleted records
+                IF EXISTS (SELECT * FROM deleted)
+                BEGIN
+                    INSERT INTO UserTypeHistories (IdUserType, UserTypeName, Modified, ModifiedBy)
+                    SELECT d.Id, d.UserTypeName, GETDATE(), 'DELETE'
+                          FROM deleted d;
+                END
+            END;
+        ");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP TRIGGER IF EXISTS trg_AuditPermission;");
+            migrationBuilder.Sql("DROP TRIGGER IF EXISTS trg_AuditPermissionUserType;");
+            migrationBuilder.Sql("DROP TRIGGER IF EXISTS trg_AuditUserType;");
+        }
+    
+
+
+    }
+  
+}
